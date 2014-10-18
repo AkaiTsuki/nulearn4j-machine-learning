0498477613718dfeb79a4d455398233389babc21
Jiachi Liu <liu.jiach@husky.neu.edu>
2014-10-18 00:59:34 -0400

    Implemented linear regression with stochastic gradient.

--- src/main/java/org/nulearn4j/linear/LinearRegression.java
+++ src/main/java/org/nulearn4j/linear/LinearRegression.java
@@ -2,14 +2,91 @@ package org.nulearn4j.linear;
 
 import org.nulearn4j.dataset.loader.DatasetLoader;
 import org.nulearn4j.dataset.matrix.Matrix;
+import org.nulearn4j.dataset.matrix.Row;
+import org.nulearn4j.dataset.validation.Validation;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by jiachiliu on 10/17/14.
  */
 public class LinearRegression {
-    public static void main(String[] args) throws Exception{
-        DatasetLoader loader = new DatasetLoader();
-        Matrix<Double> train = loader.loadBostonHousingTrain("\\s+");
-        Matrix<Double> test = loader.loadBostonHousingTest("\\s+");
+
+    private double[] weights;
+    private double learning_rate;
+    private double converged;
+    private double maxIteration;
+
+    public LinearRegression() {
+        learning_rate = 0.0001;
+        converged = 0.001;
+        maxIteration = 1;
+    }
+
+    public LinearRegression fit(Matrix<Double> train, List<Double> target) {
+        int[] dimension = train.getDimension();
+        int m = dimension[0];
+        int n = dimension[1];
+        weights = new double[n];
+        int k = 1;
+        double mse = Validation.mse(predict(train), target);
+        System.out.println("============Start fit==============");
+        while (k <= maxIteration) {
+            System.out.println("Iteration " + k + " weights: " + Arrays.toString(weights) + " mse: " + mse);
+            for (int i = 0; i < m; i++) {
+                Row<Double> row = train.getRow(i);
+                Double t = target.get(i);
+                double error = predict(row) - t;
+                for (int j = 0; j < n; j++) {
+                    weights[j] -= learning_rate * error * row.get(j);
+                }
+                System.out.println(" weights: " + Arrays.toString(weights) + " mse: " + mse);
+            }
+
+            double update = Validation.mse(predict(train), target);
+            if (Math.abs(update - mse) <= converged) {
+                System.out.println("Converged at Iteration " + k + " weights: " + Arrays.toString(weights) + " mse: " + update);
+                break;
+            }
+            mse = update;
+            k++;
+        }
+
+        return this;
+    }
+
+    public List<Double> predict(Matrix<Double> test) {
+        return test.getMatrix()
+                .stream()
+                .map(this::predict)
+                .collect(Collectors.toList());
+    }
+
+    public Double predict(Row<Double> row) {
+        double weightedSum = 0;
+        for(int i=0; i< row.size(); i++){
+            weightedSum += weights[i] * row.get(i);
+        }
+        return weightedSum;
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        Matrix<Double> train = DatasetLoader.loadBostonHousingTrain("\\s+");
+        int label = train.getColumnCount() - 1;
+        List<Double> trainTarget = train.getColumn(label);
+        train = train.removeColumn(label).addColumn(0, 1.0);
+
+        Matrix<Double> test = DatasetLoader.loadBostonHousingTest("\\s+");
+        List<Double> testTarget = test.getColumn(label);
+        test = test.removeColumn(label).addColumn(0, 1.0);
+
+        LinearRegression classifier = new LinearRegression();
+        classifier.fit(train, trainTarget);
+        List<Double> predicts = classifier.predict(test);
+
+
     }
 }
