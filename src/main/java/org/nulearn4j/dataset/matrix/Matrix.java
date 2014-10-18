63273d5ee871a6bdaff2e6cea1decff22ace44bc
Jiachi Liu <liu.jiach@husky.neu.edu>
2014-10-17 21:37:10 -0400

    init project

---
+++ src/main/java/org/nulearn4j/dataset/matrix/Matrix.java
@@ -0,0 +1,8 @@
+package org.nulearn4j.dataset.matrix;
+
+/**
+ * Created by jiachiliu on 10/17/14.
+ */
+public interface Matrix {
+
+}
---
+++ src/main/java/org/nulearn4j/dataset/matrix/Row.java
@@ -0,0 +1,42 @@
+package org.nulearn4j.dataset.matrix;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * Created by jiachiliu on 10/17/14.
+ */
+public class Row<T> {
+
+    private List<T> data;
+
+    public Row() {
+        data = new ArrayList<>();
+    }
+
+    public Row(List<T> data) {
+        this.data = data;
+    }
+
+    public Row(T[] data) {
+        this.data = Stream.of(data).collect(Collectors.toList());
+    }
+
+    public T get(int i) {
+        return data.get(i);
+    }
+
+    public int size() {
+        return data.size();
+    }
+
+    public List<T> getData() {
+        return data;
+    }
+
+    public void setData(List<T> data) {
+        this.data = data;
+    }
+}
